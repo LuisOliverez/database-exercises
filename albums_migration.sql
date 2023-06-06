75d74a0dbc8027653040a441b32f509418edf885
Luis Oliverez <louoliverez901@gmail.com>
2023-06-06 13:40:21 -0700

    added all albums >30m sales

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,10 +1,10 @@
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums(
     id INT  UNSIGNED AUTO_INCREMENT NOT NULL,
-    artist VARCHAR (50),
-    album_name VARCHAR (200),
-    release_date INT,
-    sales FLOAT,
-    genre VARCHAR (50),
+    Artist VARCHAR (50),
+    Album_Name VARCHAR (200),
+    Release_Year INT,
+    Sales_mil FLOAT,
+    Genre VARCHAR (50),
     PRIMARY KEY (id)
     );
---
+++ albums_seeder.sql
@@ -0,0 +1,3 @@
+INSERT INTO albums (artist, album_name, release_year, sales_mil, genre)
+VALUES ('Bruce Springsteen', 'Born in the U.S.A.', 1984, 30, 'Heartland Rock');
+

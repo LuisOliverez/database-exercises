9b3ebf02cf3a2bbf0359f47191a199b3a1c4d42d
Luis Oliverez <louoliverez901@gmail.com>
2023-06-06 14:01:45 -0700

    added first table info for single cmd, need second table. Basic Statements Exer #1-3B

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,3 +1,15 @@
-INSERT INTO albums (artist, album_name, release_year, sales_mil, genre)
-VALUES ('Bruce Springsteen', 'Born in the U.S.A.', 1984, 30, 'Heartland Rock');
+-- INSERT INTO albums (artist, album_name, release_year, sales_mil, genre)
+-- VALUES ('Bruce Springsteen', 'Born in the U.S.A.', 1984, 30, 'Heartland Rock');
 
+
+INSERT INTO quotes (artist, album_name, release_year, sales_mil, genre)
+VALUES ('Michael Jackson', 'Thriller', 1982, 51.2, 'Pop, Post-Disco, Funk, Rock'),
+        ('AC/DC', 'Back in Black', 1980, 30.1, 'Hard Rock'),
+        ('Whitney Houston', 'The Bodyguard', 1992, 28.7, 'R&B, Soul, Pop, Soundtrack'),
+        ('Pink Floyd', 'The Dark Side of the Moon', 1973, 24.8, 'Progressive Rock'),
+        ('Eagles', 'Their Greatest Hits(1971-1975)', 1976, 41.2, 'Country Rock, Soft Rock, Folk Rock'),
+        ('Meat Loaf', 'Bat Out of Hell', 1977, 22, 'Hard Rock, Glam ROck, Progressive Rock'),
+        ('Eagles', 'Hotel California', 1976, 31.8, 'Soft Rock'),
+        ('Shania Twain', 'Come on Over', 1997, 30.4, 'Country, Pop'),
+        ('Fleetwood Mac', 'Rumours', 1977, 29.3, 'Soft Rock'),
+        ('Bee Gees/Various Artists', 'Saturday Night Fever', 1977, 22.1, 'Disco'),

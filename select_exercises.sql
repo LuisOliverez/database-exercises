963fc548da7fb62990bf709a52d7deface72f411
Luis Oliverez <louoliverez901@gmail.com>
2023-06-07 14:00:33 -0700

    completed Select exercise #2-3A

---
+++ select_exercises.sql
@@ -0,0 +1,26 @@
+-- write queries to find the following information:
+
+-- The name of all albums by Pink Floyd.
+SELECT * FROM albums
+         WHERE artist = 'Pink floyd';
+
+
+
+-- The year Sgt. Pepper's Lonely Hearts Club Band was released
+
+
+
+-- The genre for Nevermind
+
+
+
+-- Which albums were released in the 1990s
+
+
+
+-- Which albums had less than 20 million certified sales
+
+
+
+-- All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
+-- As always, commit your changes after each step and push them out to GitHub

c42ebe2f9f8e036355aa6d21691567afc848747a
Luis Oliverez <louoliverez901@gmail.com>
2023-06-07 13:49:28 -0700

    added singles insert for albums list

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,8 +1,8 @@
--- INSERT INTO albums (artist, album_name, release_year, sales_mil, genre)
--- VALUES ('Bruce Springsteen', 'Born in the U.S.A.', 1984, 30, 'Heartland Rock');
+INSERT INTO albums (artist, album_name, release_year, sales_mil, genre)
+VALUES ('Bruce Springsteen', 'Born in the U.S.A.', 1984, 30, 'Heartland Rock');
 
 
-INSERT INTO quotes (artist, album_name, release_year, sales_mil, genre)
+INSERT INTO albums (artist, album_name, release_year, sales_mil, genre)
 VALUES ('Michael Jackson', 'Thriller', 1982, 51.2, 'Pop, Post-Disco, Funk, Rock'),
         ('AC/DC', 'Back in Black', 1980, 30.1, 'Hard Rock'),
         ('Whitney Houston', 'The Bodyguard', 1992, 28.7, 'R&B, Soul, Pop, Soundtrack'),
@@ -12,4 +12,24 @@ VALUES ('Michael Jackson', 'Thriller', 1982, 51.2, 'Pop, Post-Disco, Funk, Rock'
         ('Eagles', 'Hotel California', 1976, 31.8, 'Soft Rock'),
         ('Shania Twain', 'Come on Over', 1997, 30.4, 'Country, Pop'),
         ('Fleetwood Mac', 'Rumours', 1977, 29.3, 'Soft Rock'),
-        ('Bee Gees/Various Artists', 'Saturday Night Fever', 1977, 22.1, 'Disco'),
+        ('Bee Gees', 'Saturday Night Fever', 1977, 22.1, 'Disco'),
+        ('Led Zeppelin', 'Led Zeppelin IV', 1971, 30.4, 'Hard Rock'),
+        ('Michael Jackson', 'Bad', 1987, 21.2, 'Pop'),
+        ('Alanis Morissette', 'Jagged Little Pill', 1995, 25.4, 'Alternative Rock'),
+        ('Various Artists', 'Dirty Dancing', 1987, 24.1, 'R&B'),
+        ('Celine Dion', 'Falling Into You', 1996, 21.1, 'Pop'),
+        ('Adele', '21', 2011, 27.1, 'Soul'),
+        ('The Beatles', '1', 2000, 23.2, 'Rock'),
+        ('Metallica', 'Metallica', 1991, 22.7, 'Heavy Metal'),
+        ('Celine Dion', 'Lets Talk About Love', 1997, 20.5, 'Pop'),
+        ('Bob Marley & The Wailers', 'Legend: The Best of Bob Marley & The Wailers', 1984, 22.9, 'Reggae'),
+        ('Guns N" Roses', 'Appetite for Destruction', 1987, 22.8, 'Hard Rock'),
+        ('Bruce Springsteen', 'Born in the U.S.A.', 1984, 22, 'Heartland Rock'),
+        ('ABBA', 'Gold: Greatest Hits', 1992, 21.6, 'Disco'),
+        ('Dire Straits', 'Brother in Arms', 1985, 21.1, 'Roots Rock'),
+        ('Santana', 'Supernatural', 1999, 20.8, 'Latin Rock'),
+        ('Madonna', 'The Immaculate Collection', 1990, 19.8, 'Pop'),
+        ('Pink Floyd', 'The Wall', 1979, 18.9, 'Progressive Rock'),
+        ('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', 1967, 18.3, 'Rock'),
+        ('Michael Jackson', 'Dangerous', 1991, 17, 'R&B'),
+        ('The Beatles', 'Abby Road', 1969, 16.9, 'Rock');

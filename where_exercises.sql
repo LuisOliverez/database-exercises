de1785912e8a9ece936b393a487431ef4661b81e
Luis Oliverez <louoliverez901@gmail.com>
2023-06-07 12:03:37 -0700

    completed WHERE Clauses Exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -31,16 +31,32 @@ WHERE last_name LIKE '%q%';
 
 
 -- Add a condition to the previous query to find everybody with those names who is also male — 441 rows.
+            SELECT emp_no, first_name, last_name
+            FROM employees
+            WHERE first_name IN ('Vidya', 'Irena', 'Maya')
+               AND gender = 'M';
 
 
 
 -- Find all employees whose last name starts or ends with 'E' — 30,723 rows.
 
+            SELECT emp_no, first_name, last_name
+            FROM employees
+            WHERE last_name LIKE 'e%'
+            OR last_name LIKE '%e';
 
 
 -- Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
 
+            SELECT emp_no, first_name, last_name
+            FROM employees
+            WHERE last_name LIKE 'e%'
+               AND last_name LIKE '%e';
 
 
 -- Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
 
+            SELECT emp_no, first_name, last_name
+            FROM employees
+            WHERE last_name LIKE '%q%'
+                AND last_name NOT LIKE '%qu%';

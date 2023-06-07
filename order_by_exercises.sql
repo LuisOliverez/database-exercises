a90fbe2c6df495db75f1bee59c8eaa1da61e6ff9
Luis Oliverez <louoliverez901@gmail.com>
2023-06-07 12:43:19 -0700

    Completed Order By Exercise

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -8,15 +8,34 @@
 
 
 -- Update the query to order by first name and then last name. The first result should now be Irena Acton and the last should be Vidya Zweizig.
-
+        SELECT first_name, last_name
+        FROM employees
+        WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+        ORDER BY first_name, last_name;
 
 
 -- Change the ORDER BY clause so that you order by last name before first name. Your first result should still be Irena Acton but now the last result should be Maya Zyda.
-
+        SELECT first_name, last_name
+        FROM employees
+        WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+        ORDER BY last_name, first_name;
 
 
 -- Update your queries for employees with 'e' in their last name to sort the results by their employee number. Make sure the employee numbers are in the correct order.
-
+        SELECT  emp_no,first_name, last_name
+        FROM employees
+        WHERE last_name LIKE '%e%'
+            ORDER BY emp_no;
 
 
 -- Now reverse the sort order for both queries and compare results.
+        SELECT first_name, last_name
+        FROM employees
+        WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+        ORDER BY last_name DESC , first_name DESC ;
+
+
+        SELECT  emp_no,first_name, last_name
+        FROM employees
+        WHERE last_name LIKE '%e%'
+        ORDER BY emp_no DESC;

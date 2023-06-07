79f32866ee5124d2a12295a2b44c111acb72d120
Luis Oliverez <louoliverez901@gmail.com>
2023-06-07 11:39:24 -0700

    completed WHERE Clause pt1

---
+++ where_exercises.sql
@@ -0,0 +1,18 @@
+-- Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+
+
+-- Find all employees whose last name starts with 'E' — 7,330 rows.
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'e%';
+
+
+-- Find all employees with a 'q' in their last name — 1,873 rows.
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%';

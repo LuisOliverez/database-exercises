fead3e8c25277c90733a477b0c1d92f9562859b5
Luis Oliverez <louoliverez901@gmail.com>
2023-06-07 14:16:48 -0700

    completed Select exercise #2-3E

--- select_exercises.sql
+++ select_exercises.sql
@@ -7,20 +7,33 @@ SELECT * FROM albums
 
 
 -- The year Sgt. Pepper's Lonely Hearts Club Band was released
+SELECT release_year
+FROM albums
+    WHERE album_name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
 
-
--- The genre for Nevermind
-
+-- The genre for Supernatural
+SELECT genre
+FROM albums
+    WHERE album_name = 'Supernatural';
 
 
 -- Which albums were released in the 1990s
+SELECT release_year
+FROM albums
+    WHERE release_year BETWEEN 1990 AND 1999;
 
 
 
 -- Which albums had less than 20 million certified sales
+SELECT sales_mil
+FROM albums
+WHERE sales_mil >= 20;
+
 
 
 
 -- All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
+
+
 -- As always, commit your changes after each step and push them out to GitHub

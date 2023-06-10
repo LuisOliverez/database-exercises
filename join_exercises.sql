fdc7c49f5da15c0202d474ade4865669f64a26f0
Luis Oliverez <louoliverez901@gmail.com>
2023-06-10 12:48:17 -0700

    completed join exercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -9,7 +9,6 @@ WHERE dm.to_date = '9999-01-01'
 ORDER BY dept_name;
 
 
-
 -- Find the name of all departments currently managed by women
 
 SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Full Name'
@@ -23,12 +22,26 @@ ORDER BY dept_name;
 
 -- Find the current titles of employees currently working in the Customer Service department.
 
+SELECT t.title, COUNT(*) AS 'Total'
+FROM employees AS e
+         JOIN dept_emp AS de ON de.emp_no = e.emp_no
+         JOIN departments AS d ON d.dept_no = de.dept_no
+         JOIN titles AS t ON t.emp_no = e.emp_no
+WHERE d.dept_name = 'Customer Service'
+  AND de.to_date = '9999-01-01'
+  AND t.to_date = '9999-01-01'
+    GROUP BY t.title
+        ORDER BY Total DESC;
 
 
 
-    SELECT departments.dept_name AND 'Department Name', CONCAT(employees.first_name, ' ', employees.last_name) AS   'Department Manager', salaries.salary AS 'Salary'
-    FROM departments
-    JOIN dept_manager ON departments.dept_no - dept_manager.dept_no
-    JOIN employees ON employees.em_no = dept_manager.emp_no
-    JOIN salaries ON emp.emp_no = salaries.emp_no
-    WHERE dept_manager.to_date LIKE '9999%' AND salaries.to_date LIKE '9999%';
+
+-- Find the current salary of all current managers.
+
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name,' ',e.last_name) AS 'Department Manager', s.salary AS 'Salary'
+FROM departments AS d
+         JOIN dept_manager dm on d.dept_no = dm.dept_no
+         JOIN employees e on e.emp_no = dm.emp_no
+         JOIN salaries s on e.emp_no = s.emp_no
+WHERE dm.to_date = '9999-01-01'
+  AND s.to_date = '9999-01-01';
